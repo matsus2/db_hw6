2d22abd2cf4a1834e4b228695edf64f3ff2e3942
matsus2 <satoshi222@hotmail.com>
2016-04-14 18:11:11 -0400

    edit 1

--- hw6.py
+++ hw6.py
@@ -1,14 +1,15 @@
 import sys
+import csv
 
 if __name__ == '__main__':
-	if(len(sys.argv) != 2):
-		print("incorrect amount of arguments")
-		return 0
-	with open(sys.argv[1]) as r:
+    if(len(sys.argv) != 2):
+        print("incorrect amount of arguments")
+        sys.exit(0)
+
+    with open(sys.argv[1]) as r:
         ifile = r.readlines()
 
     input_list = []
     for line in ifile:
-    	input_list.append(line.split(','))
+        input_list.append(line.split(','))
 
-    
