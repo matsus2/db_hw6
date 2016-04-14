e0efae63d39b1016734a129c1ee466a905434acf
matsus2 <satoshi222@hotmail.com>
2016-04-14 18:45:01 -0400

    implemented an input txt

--- hw6.py
+++ hw6.py
@@ -1,6 +1,16 @@
 import sys
 import csv
 
+def method1_same_movie_idx(input_list):
+    return 0
+
+def method1_range_movie_idx(input_list):
+    return 0
+
+def method1_actor_idx(actor_list):
+    return 0
+
+
 if __name__ == '__main__':
     if(len(sys.argv) != 2):
         print("incorrect amount of arguments")
@@ -9,7 +19,13 @@ if __name__ == '__main__':
     with open(sys.argv[1]) as r:
         ifile = r.readlines()
 
-    input_list = []
     for line in ifile:
-        input_list.append(line.split(','))
+        input_list = []
+        input_list = line.split(',')
+
+        if(input_list[0] == input_list[1]):
+            method1_same_movie_idx(input_list)
+        else:
+            method1_range_movie_idx(input_list)
+
 
